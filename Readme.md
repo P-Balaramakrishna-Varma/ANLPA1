16a2798a26a848952f2be92562dda9284be1fe2f
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2023-08-26 15:57:06 +0530

    Dataset prepraration done

---
+++ Readme.md
@@ -0,0 +1,2 @@
+## References.
+- https://coderzcolumn.com/tutorials/artificial-intelligence/how-to-use-glove-embeddings-with-pytorch loading pretrained embbeding.
--- q1.py
+++ q1.py
@@ -1,5 +1,11 @@
 import nltk
 import string
+import torch
+from torch.utils.data.dataset import ConcatDataset, Dataset
+import torchtext
+from torch.utils.data import Dataset, DataLoader
+from collections import Counter, OrderedDict
+
 
 # read the entrire file into a string
 def get_tokens_from_text_corpus(file_path):
@@ -33,7 +39,47 @@ def test_get_tokens_from_text_corpus():
         for token in tokens:
             f.write(token)
             f.write('\n')
-            
-            
 
-test_get_tokens_from_text_corpus()
+# test_get_tokens_from_text_corpus()
+
+
+# Custom dataset
+class NGramDataset(Dataset):
+    def __init__(self, filename) :
+        super().__init__()
+        # Preprocessing the text corpus
+        self.tokens = get_tokens_from_text_corpus(filename)
+        
+        # Loading pretrained embedding
+        self.pretrained_embedding = torchtext.vocab.GloVe(name='6B', dim=300)
+        
+        # Creating vocabulary
+        self.vocab = torchtext.vocab.build_vocab_from_iterator(self.tokens)
+        self.vocab.set_default_index(0)
+        
+    def __len__(self):
+        return len(self.tokens) - 5
+    
+    def __getitem__(self, idx):
+        # getting pretrained embedding for previous 5 words
+        X = self.pretrained_embedding.get_vecs_by_tokens(self.tokens[idx:idx+5])
+        
+        # concatination
+        X = torch.flatten(X)
+        
+        # Target (6th word)
+        y =  self.vocab[self.tokens[idx + 5]]
+        return X, y
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+# dataset = NGramDataset('Auguste_Maquet.txt')    
+# dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+# for X, y in dataloader:
+#     print(X, "\n\n", y)
