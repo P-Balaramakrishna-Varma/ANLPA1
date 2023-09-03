897ea93adc4fea9597156efb17d282e45cf19597
balaramakrishna <balaramakrishna.p@students.iiit.ac.in>
2023-09-03 22:59:06 +0530

    Q3 done

--- q3.py
+++ q3.py
@@ -1,6 +1,6 @@
 import nltk
 import string
-import torch
+import torch, math
 import torchtext
 from torch.utils.data import Dataset, DataLoader, random_split
 import torch.nn as nn
@@ -47,10 +47,12 @@ def test_get_tokens_from_text_corpus():
 
 # Custom dataset
 class GPTDataset(Dataset):
-    def __init__(self, filename, len) :
+    def __init__(self, filename, seq_len) :
         super().__init__()
         # Preprocessing the text corpus
-        self.tokens = get_tokens_from_text_corpus(filename)[:1000]
+        self.tokens = get_tokens_from_text_corpus(filename)
+        self.tokens = self.tokens[:(int(len(self.tokens) / 2))]
+
         
         # Loading pretrained embedding
         self.pretrained_embedding = torchtext.vocab.GloVe(name='6B', dim=300)
@@ -60,7 +62,7 @@ class GPTDataset(Dataset):
         self.vocab.set_default_index(self.vocab["<unk>"])
         
         # Max sequence length
-        self.seq_len = len
+        self.seq_len = seq_len
         
     def __len__(self):
         return len(self.tokens) - self.seq_len
@@ -73,22 +75,46 @@ class GPTDataset(Dataset):
         y =  [self.vocab[token] for token in self.tokens[idx + 1 : idx + self.seq_len + 1]]
         y = torch.tensor(y)
         return X, y
- 
 
+
+def gen_pe(max_length, d_model):
+    n = 10000
+    pe = torch.zeros(max_length*d_model).reshape(max_length, d_model) 
+
+    for k in torch.arange(max_length):
+        for i in torch.arange(d_model//2):
+            theta = k / (n ** ((2*i)/d_model))       
+            pe[k, 2*i] = math.sin(theta) 
+            pe[k, 2*i+1] = math.cos(theta)
+
+    return pe
+
+
+def gen_mask(seq_len):
+    mask = torch.zeros((seq_len, seq_len)) == 0
+    for i in range(seq_len):
+        mask[i, :i + 1] = False
+    return mask
+
+ 
 class GPTLanguageModel(nn.Module):
-    def __init__(self, vocab_size):
+    def __init__(self, vocab_size, seq_len, device):
         super().__init__()
-        self.attention = nn.MultiheadAttention(embed_dim=300, num_heads=1, batch_first=True)
+        self.PE = gen_pe(seq_len, 300).to(device)
+        self.mask = gen_mask(seq_len).to(device)
+        self.attention = nn.MultiheadAttention(embed_dim=300, num_heads=3, batch_first=True)
         self.hidden2 = nn.Linear(300, vocab_size)
         self.softmax = nn.Softmax(dim=2)
+
  
     def forward(self, x):
-        # Lstm layer with 500 dim output
-        x, hidden = self.lstm(x)
+        # Decoder
+        x = x + self.PE
+        x, hidden = self.attention(x, x, x, attn_mask=self.mask)
         
-        # converts 500 dim vector into vocab_size dim vector
+        # converts 300 dim vector into vocab_size dim vector
         x = self.hidden2(x)
-        x = self.softmax(x)
+        # x = self.softmax(x)
         return x
 
 
@@ -159,8 +185,8 @@ if __name__ == "__main__":
     # hyperparameters
     device = 'cuda'
     batch_size = 1024
-    epcohs = 4
-    seq_len = 5
+    epcohs = 5
+    seq_len = 15
    
     # Data creation
     dataset = GPTDataset('Auguste_Maquet.txt', seq_len)
@@ -170,15 +196,17 @@ if __name__ == "__main__":
     test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
     # Model creation
-    Model = GPTLanguageModel(len(dataset.vocab)).to(device)
+    Model = GPTLanguageModel(len(dataset.vocab), seq_len, device).to(device)
     
     # Training
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(Model.parameters(), lr=0.1)
+    optimizer = torch.optim.Adam(Model.parameters(), lr=0.001)
     stats = []
+    print(test_loop(valid_dataloader, Model, loss_fn, device))
     for epoch in tqdm(range(epcohs)):
         train_loop(train_dataloader, Model, loss_fn, optimizer, device)
         stats.append(test_loop(valid_dataloader, Model, loss_fn, device))
+        print(stats[-1])
     plot_stats(stats)
     print(stats)
     
